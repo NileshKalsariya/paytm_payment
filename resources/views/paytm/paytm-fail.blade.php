e4cd4ca9f3d46bfb33dd235f59a799b238355d69
NileshKalsariya <nileshkalsariya6@gmail.com>
2021-04-21 11:01:13 +0530

    'paytm_integration'

---
+++ resources/views/paytm/paytm-fail.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-success alert-dismissible fade" role="alert">
+    <strong>Your Payment Has been failed</strong>
+</div>
+<a href="{{route('paytm.purchase')}}">Check the demo again</a>
---
+++ resources/views/paytm/paytm-success-page.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-success alert-dismissible fade" role="alert">
+    <strong>Payment Has been Successfully Received</strong>
+</div>
+<a href="{{route('paytm.purchase')}}">Check the demo again</a>
